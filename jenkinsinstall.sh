9e7fcfcacc8f5a5638f3bc2eb4e97282e56b26ae
prem <prem.dayal.singh1996@gmail.com>
2020-09-05 23:48:38 -0700

    updated

---
+++ jenkinsinstall.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
